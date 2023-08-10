c56add5e17d25faf2b5b47f7d2804239814b5798
Mitchkal <mitchellkalenda@gmail.com>
2023-08-09 22:34:53 -0400

    fixed formatting

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for asciit in range(ord('a'), ord('z')+1):
     if asciit != ord('q') and asciit != ord('e'):
-        print(chr(asciit), end="")
+        print(f"{chr(asciit)}", end="")
