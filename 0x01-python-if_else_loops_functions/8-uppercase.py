f73a52f38153365bedf140a1d1f257862bc5d780
Mitchkal <mitchellkalenda@gmail.com>
2023-08-10 19:34:32 -0400

    fixed error forempty input

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if len(str) == 0:
-        print("error")
-        return
+
     j = 0
     for c in str:
         if ord(c) in range(97, 123):
             c = chr(ord(c) - 32)
         print("{}".format(c), end="\n" if j == len(str) - 1 else "")
         j = j + 1
+    else:
+        print(" ")
