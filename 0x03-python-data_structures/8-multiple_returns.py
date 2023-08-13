3cd753f08fbd95a86431332162ae396f52af2ace
Mitchkal <mitchellkalenda@gmail.com>
2023-08-13 04:48:12 -0400

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if sentence is None:
+        first = None
+    else:
+        first = sentence[0]
+    return (length, first)
