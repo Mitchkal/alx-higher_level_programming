a0b5ad890468c56616ca83fea7146f345de2c155
Mitchkal <mitchellkalenda@gmail.com>
2023-08-13 07:53:32 -0400

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    i = 0
+    new = my_list.copy()
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            new[i] = True
+        else:
+            new[i] = False
+    return(new)
