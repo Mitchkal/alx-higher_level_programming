e96387aa6e6bc1fe0a7efe26059069c7852c8b9a
Mitchkal <mitchellkalenda@gmail.com>
2023-08-29 16:25:34 -0400

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+module 2-square
+defines square class
+"""
+
+
+class Square:
+    """
+    square class
+    Attributes:
+        size  : square side
+    """
+    def __init__(self, size=0):
+        """
+        initializes the square class
+        Arguments:
+            size : the sde
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self .__size = size
