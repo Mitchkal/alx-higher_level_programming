077d9a4ff7dc7482b637c4c65de0e2d7ab6b7c02
Mitchkal <mitchellkalenda@gmail.com>
2023-08-29 15:33:47 -0400

    fixed documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+Module 0-square
+defines a square; an empty class
+"""
+
+
 class Square:
-    """
-    defines a square; an empty class
-    """
+    """Empty class"""
     pass
