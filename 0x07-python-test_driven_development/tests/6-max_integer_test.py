ec3dc46d5207212069440bf448fdf921b2148246
Mitchkal <mitchellkalenda@gmail.com>
2023-09-06 10:26:05 -0400

    unitests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,10 +11,12 @@ class TestMaxInteger(unittest.TestCase):
 
     """tests max_integer"""
     def test_module_docstring(self):
+        """checks module documentation"""
         doc = __import__('6-max_integer').__doc__
         self.assertTrue(len(doc) > 1)
 
     def test_function_docstring(self):
+        """checks function documentation"""
         func_doc = __import__('6-max_integer').max_integer.__doc__
         self.assertTrue(len(func_doc) > 1)
 
@@ -29,6 +31,10 @@ class TestMaxInteger(unittest.TestCase):
             max_integer([False, None])
         with self.assertRaises(TypeError):
             max_integer([5, "str"], None)
+        with self.assertRaises(TypeError):
+            max_integer([1, 2, '6', 4.0], 4.0)
+        with self.assertRaises(TypeError):
+            max_integer(["1234", 'z'], '4')
 
     def test_max(self):
         """comfirms valueerrors"""
@@ -38,18 +44,17 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-1, -2, -5, -2, -4]), -1)
         self.assertEqual(max_integer([-1, 5, -2, 3]), 5)
         self.assertEqual(max_integer([5, 5, 3, 2, 1]), 5)
-        self.asserEqual(max_integer([10]), 10)
+        self.assertEqual(max_integer([10]), 10)
         self.assertEqual(max_integer([1.1, 1.2, 3.1, 4.2]), 4.2)
-        self.assertEqual(max_integer([1, 2, '6', 4.0]), 4.0)
-        self .asertEqual(max_integer([i for i in range(10**6)]), 999999)
+        self.assertEqual(max_integer([i for i in range(10**6)]), 999999)
         self.assertEqual(max_integer([1, 2, -float('inf'), 3, 4]), 4)
         self.assertEqual(max_integer("1234"), '4')
         self.assertEqual(max_integer("abcd"), 'd')
-        self.assertEqual(max_integer(["1234", 'z']), '4')
-        self.assertEqual(max_integer(["abcd", 'z'], 'z'))
+        self.assertEqual(max_integer(["abcd", 'z']), 'z')
 
         def test_lists(self):
-            self.AsserEqual(max_integer(['name', 'banana']), 'name')
+            """confirns list inputs"""
+            self.assertEqual(max_integer(['name', 'banana']), 'name')
 
     if __name__ == "__main__":
         unittest.main()
