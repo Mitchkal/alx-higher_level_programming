18d8e836d0c10657122b05ef7c53e7237380e3fe
Mitchkal <mitchellkalenda@gmail.com>
2023-09-12 14:00:13 -0400

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""module 4-from_json_string"""
+
+
+def from_json_string(my_str):
+    """
+    retrns json representation of object
+    """
+    import json
+
+    return (json.loads(my_str))
