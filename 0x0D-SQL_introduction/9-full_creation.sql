7ac789c1275ce32dc1113000835d5c00548091c7
Mitchkal <mitchellkalenda@gmail.com>
2023-10-17 20:29:43 -0400

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,10 @@
---creates 'second_table' and adds multi rows
+-- create table and fill
 
 CREATE TABLE IF NOT EXISTS second_table (
-	id INT,
-	name VARCHAR(256),
-	score INT
+       id INT,
+       name VARCHAR(256),
+       score INT
 );
-
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES
 (1, 'John', 10),
 (2, 'Alex', 3),
