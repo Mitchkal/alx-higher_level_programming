6a934193863d817e8b3bb15857be85d7ae7a1f08
Mitchkal <mitchellkalenda@gmail.com>
2023-11-13 16:26:08 -0500

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ const [, , arg1] = process.argv;
 const parsedNumber = parseInt(arg1);
 
 if (!isNaN(parsedNumber)) {
-  console.log('My number: ', parsedNumber);
+  console.log('My number:', parsedNumber);
 } else {
   console.log('Not a number');
 }
