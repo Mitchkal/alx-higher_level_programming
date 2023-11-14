0082fa42878e1a73b38e8b8e5496a46d419ad9a6
Mitchkal <mitchellkalenda@gmail.com>
2023-11-14 13:11:27 -0500

    objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!usr/bin/node
+const oldSquare = require('./5-square');
+
+class Square extends oldSquare {
+  charPrint (c) {
+    const printChar = c || 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(printChar.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
