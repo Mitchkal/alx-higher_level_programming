b840bd1f740e34ac231436c3ad10637b84b1cbd6
Mitchkal <mitchellkalenda@gmail.com>
2023-11-30 09:15:28 -0500

    0-body_size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#takes URL, ends request to it, and displays size of body response
+
+
+url=$1
+response=$(curl -sI $url)
+
+content_length=$(echo "$response" | grep -i 'Content-Length' | awk '{print $2}' | tr -d '\r\n')
+
+echo $content_length
