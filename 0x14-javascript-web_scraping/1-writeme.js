06a0ad4eb4ef274900a37b217e0acc279da82747
Mitchkal <mitchellkalenda@gmail.com>
2024-01-03 15:58:26 -0500

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const file = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(file, data, 'utf-8', function (err) {
+  if (err) {
+    console.error(err);
+  }
+});
